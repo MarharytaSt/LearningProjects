d07d72c3ed6ceb2b6b62b17646c6c2a73b9383cb
MarharytaSt <m.stukachova@gmail.com>
2025-08-08 23:35:11 +0200

    Create a validation for input fields

--- own-project-employees-app-FE/src/components/employees-add-form/employees-add-form.js
+++ own-project-employees-app-FE/src/components/employees-add-form/employees-add-form.js
@@ -13,25 +13,56 @@ class EmployeesAddForm extends Component {
         }
     }
 
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const {firstName, lastName, salary} = this.state;
+
+        if(firstName === '' || lastName === '' || !salary){
+            alert('Пожалуйста, заполните поля!');
+            return;
+        }else if(salary < 0){
+            alert('Введите положительное число!');
+            return;
+        }
+
+        // this.props.onAdd(this.state.firstName, this.state.lastName, this.state.salary);
+        // this.setState({
+        //     firstName: '',
+        //     lastName: '',
+        //     salary: ''
+        // })
+    }
+
     render() {
         const {firstName, lastName, salary} = this.state;
 
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
-                <form className="add-form">
+                <form className="add-form"
+                onSubmit={this.onSubmit}>
                     <input type="text" 
                         placeholder="Введите имя"
                         name="firstName"
-                        value={firstName}/>
+                        value={firstName}
+                        onChange={this.onValueChange}/>
                     <input type="text" 
                         placeholder="Введите фамилию"
                         name="lastName"
-                        value={lastName}/>
-                    <input type="text" 
+                        value={lastName}
+                        onChange={this.onValueChange}/>
+                    <input type="number" 
                         placeholder="Зарплата"
                         name="salary"
-                        value={salary}/>
+                        value={salary}
+                        onChange={this.onValueChange}/>
 
                     <button type="submit">Добавить</button>
                 </form>
