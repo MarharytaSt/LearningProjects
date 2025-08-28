32a122023cb996bba8331e48e063f95c15a180cd
MarharytaSt <m.stukachova@gmail.com>
2025-08-28 18:12:38 +0200

    Create integration front app with back app

--- receipt-book-be/src/index.js
+++ receipt-book-be/src/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
 
-
+app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json('Hello World!');
 });
 
 app.listen(PORT, () => {
--- receipt-book-fe/src/components/app/app.js
+++ receipt-book-fe/src/components/app/app.js
@@ -1,12 +1,26 @@
-
 import './app.css';
+import apiServices from '../../services/apiServices';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+
+  test = async () => {
+
+    const response = await apiServices.getAsync('/');
+
+    console.log(response);
+    
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.test}>Test</button>
+      </div>
+    );
+  }
 }
 
 export default App;
