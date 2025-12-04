c63437bd1bbb0ecccd219213d08ffd04be3ba363
MarharytaSt <m.stukachova@gmail.com>
2025-12-04 16:26:36 +0100

    Adding a ref

--- marvel-project/src/components/charList/CharList.js
+++ marvel-project/src/components/charList/CharList.js
@@ -37,7 +37,7 @@ class CharList extends Component {
 
     onCharListLoaded = (newCharList) => {
         let ended = false;
-        if(newCharList.length < 9) {
+        if (newCharList.length < 9) {
             ended = true;
         }
 
@@ -59,9 +59,22 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        if (ref) {
+            this.itemRefs.push(ref);
+        }
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
 
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { 'objectFit': 'cover' };
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = { 'objectFit': 'unset' };
@@ -70,8 +83,19 @@ class CharList extends Component {
             return (
                 <li
                     className="char__item"
+                    tabIndex={0}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    ref={this.setRef}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === '' || e.key === 'Enter') {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className="char__name">{item.name}</div>
                 </li>
@@ -101,7 +125,7 @@ class CharList extends Component {
                 <button
                     className="button button__main button__long"
                     disabled={newItemLoading}
-                    style={{'display': charEnded ? 'none' : 'block'}}
+                    style={{ 'display': charEnded ? 'none' : 'block' }}
                     onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
