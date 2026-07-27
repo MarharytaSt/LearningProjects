e3a9a452bd7e92ca8a5986cca21292b39e0774b1
MarharytaSt <m.stukachova@gmail.com>
2026-07-27 22:26:29 +0200

    Declaration of types and fixing bugs

--- events/server/src/db/entities/event-participant.entity.ts
+++ events/server/src/db/entities/event-participant.entity.ts
@@ -3,7 +3,7 @@ import { Event } from "./event.entity";
 import { User } from "./user.entity";
 
 
-@Entity('enent_participants')
+@Entity('event_participants')
 @Index('UQ_EVENT_PARTICIPANTS_EVENT_USER', ['eventId', 'userId'], { unique: true })
 export class EventParticipant {
     @PrimaryGeneratedColumn('uuid')
