c3013ad2c8902a6f9410d68b6459775bec2145b6
MarharytaSt <m.stukachova@gmail.com>
2026-07-30 21:37:33 +0200

    Joining the event

--- events/server/src/modules/events/events.routes.ts
+++ events/server/src/modules/events/events.routes.ts
@@ -131,7 +131,53 @@ export const eventsRoutes: FastifyPluginAsync = async (app) => {
             })
         }
 
-        await eventRepository.delete({id: event.id})
+        await eventRepository.delete({ id: event.id })
         return reply.code(204).send()
+    })
+
+    app.post<{ Params: EventParams }>('/:id/join', { preHandler: [app.authenticate] }, async (request, reply) => {
+        const event = await eventRepository.findOne({where: {id: request.params.id}})
+
+        if (!event) {
+            return reply.code(404).send({ message: 'Событие не найдено' })
+        }
+
+        if (event.ownerId === request.user.sub) {
+            return reply.code(400).send({
+                message: 'Нельзя присоединиться к своему событию'
+            })
+        }
+
+        const existingParticipation = await participantsRepository.findOne({
+            where: {eventId: event.id, userId: request.user.sub}
+        })
+
+        if(existingParticipation) {
+            return reply.code(409).send({
+                message: 'Вы уже присоединились к собыию'
+            })
+        }
+
+        const participationCount = await participantsRepository.count({
+            where: {eventId: event.id}
+        })
+
+        if(participationCount >= event.capacity) {
+            return reply.code(409).send({
+                message: 'Свободных мест нет'
+            })
+        }
+
+        const participation = participantsRepository.create({
+            eventId: event.id,
+            userId: request.user.sub
+        })
+
+        const savedParticipation = await participantsRepository.save(participation)
+
+        return reply.code(201).send({
+            message: 'Вы присоединились к событию',
+            participation: savedParticipation
+        })
     })
 }
