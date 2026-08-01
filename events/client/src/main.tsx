24cb42d78a598786f57129c3510993ab688d14d1
MarharytaSt <m.stukachova@gmail.com>
2026-08-01 02:22:38 +0200

    Setting up React Router

--- events/client/src/main.tsx
+++ events/client/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './tailwind.css'
+import { AppRouterProvider } from './app/app-router-provider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppRouterProvider />
   </React.StrictMode>
 
 )
