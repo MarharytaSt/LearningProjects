bcc6af3c29315735b5554caade93dc88c00f4f6d
MarharytaSt <m.stukachova@gmail.com>
2026-08-07 18:37:41 +0200

    Implementing functionality for event queries

--- events/client/src/lib/utils.ts
+++ events/client/src/lib/utils.ts
@@ -11,17 +11,27 @@ export function getApiErrorMessage(
   error: unknown,
   fallback = 'Что-то пошло не так...'
 ): string {
-  if(isAxiosError<ApiErrorResponse>(error)){
+  if (isAxiosError<ApiErrorResponse>(error)) {
     const data = error.response?.data;
 
-    if(data && typeof data.message === 'string') {
+    if (data && typeof data.message === 'string') {
       return data.message;
     }
   }
 
-  if(error instanceof Error) {
+  if (error instanceof Error) {
     return error.message;
   }
 
   return fallback;
+}
+
+export function getUserInitials(name: string) {
+  const parts = name.trim().split(/\s+/)
+
+  if (parts.length === 0) return '?'
+  if (parts.length === 1) return parts[0].slice(0, 2).toUpperCase()
+
+
+  return (parts[0]![0!] + parts[parts.length - 1]![0]).toLocaleUpperCase()
 }
