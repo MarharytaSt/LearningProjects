6a45fe0b6ed15d7a0bf530b410e17844c9219928
MarharytaSt <m.stukachova@gmail.com>
2026-08-12 18:05:08 +0200

    Implementing functionality for the "My Events" page

--- events/client/src/app/router.tsx
+++ events/client/src/app/router.tsx
@@ -9,6 +9,7 @@ import { EventsAllPage } from "@/pages/events/all/page";
 import { EventsNewPage } from "@/pages/events/new/page";
 import { EventDetailsPage } from "@/pages/events/details/page";
 import { EventsEditPage } from "@/pages/events/edit/page";
+import { EventsMyPage } from "@/pages/events/my/page";
 
 export const appRouter = createBrowserRouter([
     {
@@ -27,7 +28,7 @@ export const appRouter = createBrowserRouter([
                 element: <ProtectedRoute />,
                 children: [
                     { path: "events", element: <EventsAllPage/> },
-                    { path: "events/my", element: <h1>events/my</h1> },
+                    { path: "events/my", element: <EventsMyPage/> },
                     { path: "events/new", element: <EventsNewPage/> },
                     { path: "events/:id", element: <EventDetailsPage/> },
                     { path: "events/:id/edit", element: <EventsEditPage/> }
